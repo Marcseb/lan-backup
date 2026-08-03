6f34a703e53ba3eedc1383b8e21fe6245e11aa92
Marcseb <66306604+Marcseb@users.noreply.github.com>
2026-08-03 14:25:27 +0200

    feat: register admin router

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import unlockRouter from "./unlock";
+import adminRouter from "./admin";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use(unlockRouter);
+router.use(adminRouter);
 
 export default router;
